158f5e1a35461633df0e8da380d6f8c10437dc8a
Changhyun Kwon <chkwon@usf.edu>
2016-04-07 14:03:56 -0400

    first algorithm implementations and tests

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -1,4 +1,4 @@
-function _solve_fp(m, step_size, tolerance, max_iter)
+function _solve_fp(m, x0, step_size, tolerance, max_iter)
 
     F = getJuVIData(m).F
     var = getJuVIData(m).var
@@ -6,9 +6,8 @@ function _solve_fp(m, step_size, tolerance, max_iter)
     dim = length(F)
 
     # initialization
-    xk = zeros(dim)
-    # xk = [30, 50, 20]
-    setValue(var, xk)
+    xk = x0
+    setValue(var, x0)
     fk = getValue(F)
     tau = step_size
 
@@ -20,7 +19,7 @@ function _solve_fp(m, step_size, tolerance, max_iter)
         new_fk = getValue(F)
         rel_err = norm(xk-new_xk) / norm(xk)
 
-        @printf("iteration %4d: rel_err=%10.7f\n", i, rel_err)
+        @printf("iteration %4d: rel_err=%15.12f\n", i, rel_err)
 
         if  rel_err < tolerance
             break
@@ -30,5 +29,49 @@ function _solve_fp(m, step_size, tolerance, max_iter)
         fk = new_fk
     end
 
-    return xk
+    return xk, fk
+end
+
+
+
+# Extragradient Section 12.1.2 of Facchinei and Pang (2003)
+function _solve_eg(m, x0, step_size, tolerance, max_iter)
+
+    F = getJuVIData(m).F
+    var = getJuVIData(m).var
+    @assert length(F) == length(var)
+    dim = length(F)
+
+    # initialization
+    xk = x0
+    setValue(var, x0)
+    fk = getValue(F)
+    tau = step_size
+
+    for i=1:max_iter
+        @setNLObjective(m, Min, sum{ ( var[i] - (xk[i]-tau*fk[i]) )^2, i=1:dim} )
+        status = solve(m)
+        # @assert status == :optimal
+        mid_xk = getValue(var)
+        mid_fk = getValue(F)
+
+        @setNLObjective(m, Min, sum{ ( var[i] - (xk[i]-tau*mid_fk[i]) )^2, i=1:dim} )
+        status = solve(m)
+        # @assert status == :optimal
+        new_xk = getValue(var)
+        new_fk = getValue(F)
+
+        rel_err = norm(xk-new_xk) / norm(xk)
+
+        @printf("iteration %4d: rel_err=%15.12f\n", i, rel_err)
+
+        if  rel_err < tolerance
+            break
+        end
+
+        xk = new_xk
+        fk = new_fk
+    end
+
+    return xk, fk
 end
--- src/model.jl
+++ src/model.jl
@@ -28,15 +28,39 @@ function setVIP(m::Model, expressions::Array{JuMP.NonlinearExpression,1}, variab
     return m
 end
 
-function solveVIP(m::Model; step_size=0.01,
-                            algorithm=:fixed_point,
-                            tolerance=1e-10,
-                            max_iter=100            )
+
+function solveVIP(m::Model    ; step_size=0.01,
+                                algorithm=:fixed_point,
+                                tolerance=1e-5,
+                                max_iter=1000            )
+
+    F = getJuVIData(m).F
+    var = getJuVIData(m).var
+    @assert length(F) == length(var)
+    dim = length(F)
+
+    x0 = zeros(dim)
+
+    return solveVIP(m, x0, step_size=step_size, algorithm=algorithm, tolerance=tolerance, max_iter=max_iter)
+
+end
+
+
+
+function solveVIP(m::Model, x0; step_size=0.01,
+                                algorithm=:fixed_point,
+                                tolerance=1e-5,
+                                max_iter=1000            )
     # :fixed_point basic fixed-point iteration, Section 12.1.1
     # :extra_gradient extragradient, Section 12.1.2
 
+    sol = []
     if algorithm==:fixed_point
-        _solve_fp(m, step_size, tolerance, max_iter)
+        sol, F = _solve_fp(m, x0, step_size, tolerance, max_iter)
+    elseif algorithm==:extra_gradient
+        sol, F = _solve_eg(m, x0, step_size, tolerance, max_iter)
     end
+
+    return sol, F
 
 end
